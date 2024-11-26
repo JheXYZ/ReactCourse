b818d0c3451ae5b1ce98febe89a36b99179c8c68
JheX <nicorodriguez242003@gmail.com>
2024-11-26 12:18:49 -0300

    currently not working

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,7 +6,7 @@ export default function Button({
   widening = false,
   disabled = false,
   styles,
-  extraStyles,
+  extraStyles = "",
 }) {
   function handleClick() {
     if (!onClick || disabled) return;
@@ -14,9 +14,16 @@ export default function Button({
     else onClick();
   }
 
-  const style =
+  let style =
     styles ||
-    `rounded-lg border-2 border-accent bg-gradient-to-tr from-background to-secondary disabled:saturate-0 disabled:cursor-not-allowed px-4 py-2 text-text transition-all ${widening && !disabled && "hover:tracking-wider"} ${underline && "hover:underline"} ${!disabled && "hover:font-medium hover:shadow-md hover:shadow-accent hover:brightness-110 active:scale-95 active:brightness-100"} ${extraStyles}`;
+    `rounded-lg border-2 border-accent bg-gradient-to-tr from-background to-secondary disabled:saturate-0 disabled:cursor-not-allowed px-4 py-2 text-text transition-all ${extraStyles}`;
+
+  if (!disabled) {
+    style +=
+      "hover:font-medium hover:shadow-md hover:shadow-accent hover:brightness-110 active:scale-95 active:brightness-100";
+    if (widening) style += "hover:tracking-wider";
+  }
+  if (underline) style += "hover:underline";
 
   return (
     <button className={style} onClick={handleClick} disabled={disabled}>
---
+++ src/context/ProductsContext.jsx
@@ -0,0 +1,11 @@
+import { useEffect, useState } from "react";
+
+export default function ProductsContext({ children }) {
+    const [products, setProducts] = useState([]);
+    
+    useEffect(() => {
+        
+    }, []);
+
+
+}
